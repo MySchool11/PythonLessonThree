4c3f139d5d662b4ac57a749a5fb40178c595e574
Samuel Bancroft <samuelbancroft@hotmail.com>
2017-05-16 17:25:42 +0100

    Added comments to explain program.

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import test
 __author__ = "Samuel Bancroft"
-print('''All variables in Python must star with a letter (upper or 
+print('''All variables in Python must star with a letter (upper or          
 lower case) or an underscore. you cannot use a number at the start
-or special characters anywhere in the name''')
-print()
+or special characters anywhere in the name''')                          # Print information about variables
+print()                                                                 # Print blank line
 print('''valid names include:
 _hello, _Hello, hello_world, Hello_world, _123, _a1, a123
 invalid names include:
-!hello, 123, $help, **123, _!help, _%^test, _help%''')
-print()
+!hello, 123, $help, **123, _!help, _%^test, _help%''')                  # Print information about variables
+print()                                                                 # Print blank line
 print('''Another thing to remember is case sensitivity,
-so Hello is different from hello''')
-print()
-print("Try it out, enter a variable name and see if it is valid:")
-checkName = "blank"
-while checkName:
-    checkName = input("press enter on its own when you are finished: ")
-    if checkName:
-        test.check(checkName)
+so Hello is different from hello''')                                    # Print information about variables
+print()                                                                 # Print blank line
+print("Try it out, enter a variable name and see if it is valid:")      # Print instructions
+checkName = "blank"                                                     # Set checkName string to arbitrary value so initial while check passes
+while checkName:                                                        # While checkName uses a boolean check because an empty string is false and a populated one true in Python
+    checkName = input("press enter on its own when you are finished: ") # Input the variable name into checkName string
+    if checkName:                                                       # Check variable contains something to prevent odd error message when pressing enter alone to exit while loop
+        test.check(checkName)                                           # Pass the variable name string to the check() method in the test.py file
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-def check(stringToCheck):
-    if stringToCheck.isidentifier():
-        print("Valid variable name in Python 3.x")
-    else:
-        print("Invalid variable name in Python 3.x")
+__author__ = "Samuel Bancroft"
+# You can ignore this file if you do not feel able to grasp what is happening here
+# If you want to know, read on
+def check(stringToCheck):                               # This defines a function call check which needs a string passed to it
+    if stringToCheck.isidentifier():                    # If statement checks whether the passed string is a valid variable name using the inbuilt function isidentifier()
+        print("Valid variable name in Python 3.x")      # If valid print statement confirming validity
+    else:                                               # Else
+        print("Invalid variable name in Python 3.x")    # Print statement confirming name is invalid
